2aa108226698776f0697c24c68d5e9e81add42f6
CleuJunior <cleu.junior@gmail.com>
2021-09-11 18:09:16 -0300

    9 - Cadastrando Projetos

--- src/App.js
+++ src/App.js
@@ -27,12 +27,19 @@ function App(){
 
     }, [projects]);
 
-    function handleAddProject(){
+    async function handleAddProject(){
         // projects.push(`Novo projeto ${Date.now()}`);
 
-        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+        // setProjects([...projects, `Novo projeto ${Date.now()}`]);
 
-        console.log(projects);
+        const response = await api.post('/projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "React API"
+        })
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
     }
 
     return (
